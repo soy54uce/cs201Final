cf8384c0150a2de228c97c1a7b8c979419cbbcfd
soy54uce <beaudfin@gmail.com>
2018-11-25 16:32:28 -0500

    add event init code

--- simulator.bbeaudin.c
+++ simulator.bbeaudin.c
@@ -4,12 +4,9 @@
 #include <stdlib.h>
 #define TIME_QUANTUM 4
 
-int cpuIdIdle;
-PQueue eventQueue;
-PQueue cpuQueue;
 
 void handleEvent(Event event) {	
-	switch (event.eventType) {
+	switch (event->eventType) {
 		case PROCESS_SUBMITTED:
 			if (cpuIsIdle) {
 				Event *nextEvent = (Event *) malloc(sizeof(Event));
@@ -24,8 +21,43 @@ void handleEvent(Event event) {
 }
 
 int main() {
-	
+	int cpuIdIdle;
+	PQueue eventQueue;
+	PQueue cpuQueue;
+	DataNode *data;
 
+	eventQueue.head = NULL;
+	eventQueue.tail = NULL;
+	cpuQueue.head = NULL;
+	cpuQueue.tail = NULL;
+	
+	Process *p1 = (Process *) malloc(sizeof(Event));
+	Process *p2 = (Process *) malloc(sizeof(Event));
+	Process *p3 = (Process *) malloc(sizeof(Event));
+	Process *p4 = (Process *) malloc(sizeof(Event));
+	Process *p5 = (Process *) malloc(sizeof(Event));
+	
+	Event *e1 = (Event *) malloc(sizeof(Event));
+	Event *e2 = (Event *) malloc(sizeof(Event));
+	Event *e3 = (Event *) malloc(sizeof(Event));
+	Event *e4 = (Event *) malloc(sizeof(Event));
+	Event *e5 = (Event *) malloc(sizeof(Event));
+	e1->eventType = PROCESS_SUBMITTED;
+	e1->process = p1;
+	e2->eventType = PROCESS_SUBMITTED;
+	e2->process = p1;
+	e3->eventType = PROCESS_SUBMITTED;
+	e3->process = p1;
+	e4->eventType = PROCESS_SUBMITTED;
+	e4->process = p1;
+	e5->eventType = PROCESS_SUBMITTED;
+	e5->process = p1;
+	enqueue(&eventQueue, 1, e1);
+	enqueue(&eventQueue, 2, e2);
+	enqueue(&eventQueue, 3, e3);
+	enqueue(&eventQueue, 4, e4);
+	enqueue(&eventQueue, 5, e5);
+	
 	currentTime = getMinPriority(eventQueue);
 	event = dequeue(eventQueue);
 	while (event != NULL && currentTime < RUN_TIME) {
--- simulator.bbeaudin.h
+++ simulator.bbeaudin.h
@@ -1,22 +1,3 @@
-typedef struct {
-	int pid;
-	int burstTime;
-	int waitTime;
-	int numPreemtions;
-	int lastTime;
-} Process;
-
-typedef struct {
-	EventType eventType;
-	Process *process;
-} Event;
-
-typedef enum EventTypeEnum {
-	PROCESS_SUBMITTED,
-	PROCESS_STARTS,
-	PROCESS_ENDS,
-	PROCESS_TIMESLICE_EXPIRES
-} EventType;
 
 // check event type and process accordingly
 void handleEvent(Event event);
