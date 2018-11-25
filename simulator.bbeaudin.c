62a78b922fae55efeb24e296151782aa78890af7
soy54uce <beaudfin@gmail.com>
2018-11-25 17:35:47 -0500

    move pqueue definitions within simulator files for easier customization

--- simulator.bbeaudin.c
+++ simulator.bbeaudin.c
@@ -1,11 +1,73 @@
-#include "pqueue.bbeaudin.h"
 #include "simulator.bbeaudin.h"
 #include <stdio.h>
 #include <stdlib.h>
 #define TIME_QUANTUM 4
 
+int enqueue(PQueue *myQ, int priority, DataNode *data) {
+  PQueueNode *newNode = (PQueueNode *) malloc(sizeof(PQueueNode));
+  newNode->priority = priority;
+  newNode->data = data;
+  if (myQ->head ==  NULL) {
+    newNode->next = NULL;
+    myQ->head = newNode;
+    myQ->tail = newNode;
+    return(0);
+  } else {
+    PQueueNode *currentNode = myQ->head;
+    PQueueNode *prevNode = NULL;
+    while (currentNode != NULL && currentNode->priority <= priority) {
+        prevNode = currentNode;
+	currentNode = currentNode->next;
+    }
+    if (prevNode) {
+	prevNode->next = newNode;
+    } else {
+        myQ->head = newNode;
+    }
+    newNode->next = currentNode;
+    if (newNode->next == NULL) {
+	myQ->tail = newNode;
+    }
+  }
+  return(0);
+}
+
+DataNode * dequeue(PQueue *myQ) {
+  PQueueNode *currentNode = myQ->head;
+  DataNode *data = NULL;
+  if (currentNode != NULL) {
+    data = currentNode->data;
+    myQ->head = currentNode->next;
+    free(currentNode);
+  }
+  return data;
+}
+
+int printQueue(PQueue *myQ) {
+  PQueueNode *currentNode = myQ->head;
+  while (currentNode != NULL) {
+    printf("| %d %s |\n", currentNode->priority, currentNode->data->name);
+    currentNode = currentNode->next;
+  }	
+  return(0);
+}
+
+void * peek(PQueue *myQ) {
+  if(myQ->head != NULL) {
+	return myQ->head->data;
+  }
+  return NULL;
+}
+
+int getMinPriority(PQueue *myQ) {
+  int low = -1;
+  if (myQ->head != NULL) 
+    low = myQ->head->priority;
+  return low;
+}
+
 void handleEvent(Event myEvent) {	
-	switch (event->eventType) {
+	switch (myEvent->eventType) {
 		case PROCESS_SUBMITTED:
 			if (cpuIsIdle) {
 				Event *nextEvent = (Event *) malloc(sizeof(Event));
@@ -51,10 +113,15 @@ int main() {
 	e4->process = p1;
 	e5->eventType = PROCESS_SUBMITTED;
 	e5->process = p1;
-	enqueue(&eventQueue, 1, e1);
+	data->event = e1;
+	enqueue(&eventQueue, 1, data);
+	data->event = e2;
 	enqueue(&eventQueue, 2, e2);
+	data->event = e3;
 	enqueue(&eventQueue, 3, e3);
+	data->event = e4;
 	enqueue(&eventQueue, 4, e4);
+	data->event = e5;
 	enqueue(&eventQueue, 5, e5);
 	
 	currentTime = getMinPriority(eventQueue);
--- simulator.bbeaudin.h
+++ simulator.bbeaudin.h
@@ -1,3 +1,57 @@
+
+typedef struct {
+  Event *event;
+  Process *process;
+} DataNode;
+
+typedef struct PQN {
+  int priority;
+  DataNode *data;
+  struct PQN *next;
+} PQueueNode;
+
+typedef struct {
+  PQueueNode *head;
+  PQueueNode *tail;
+} PQueue;
+
+typedef struct {
+ int pid;
+ int burstTime;
+ int waitTime;
+ int numPreemptions;
+ int lastTime;
+} Process;
+
+typedef struct {
+ EventType eventType;
+ Process *process;
+} Event;
+
+typedef enum EventTypeEnum {
+ PROCESS_SUBMITTED,
+ PROCESS_STARTS,
+ PROCESS_ENDS,
+ PROCESS_TIMESLICE_EXPIRES
+} EventType;
+
+int enqueue(PQueue *pqueue, int priority, DataNode *data);
+// put node in queue in priority order; in case of tie in priority,
+// then put node after all nodes with the specified priority
+
+DataNode * dequeue(PQueue *pqueue);
+// return NULL is queue is empty; otherwise return first node in queue
+
+int printQueue(PQueue *pqueue);
+// print queue from head to tail
+
+void * peek(PQueue *pqueue);
+// return the first node in the queue, but do not remove it; return NULL
+// if queue is empty
+
+int getMinPriority(PQueue *pqueue);
+// return priority of the first node in queue; return -1 if queue is empty
+
 // check event type and process accordingly
 void handleEvent(Event myEvent);
 
